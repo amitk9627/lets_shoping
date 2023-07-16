80f5bcfe4cad55f131c58ed6c3634a835836afdd
Amit kumar <ramitgzp56@gmail.com>
2023-07-17 01:41:59 +0530

    cart ui delete reset item cart

--- src/container/Cart.jsx
+++ src/container/Cart.jsx
@@ -2,11 +2,27 @@ import React, { useContext } from 'react'
 import { ContextState } from '../context/Context'
 
 const Cart = () => {
-  const {state} = useContext(ContextState);
-  console.log(state);
+  const { state,dispatch } = useContext(ContextState);
+ 
   return (
-    <div>
+    <div className='cart-section'>
+      <div className='cartTotal'>
+        <div>Total Amount</div>
+        <div>$783</div>
+      </div>
+      {state.length>0  ? <button onClick={()=>dispatch({type:"Reset"})}>Empty</button>: ""}
+      
 
+      <div className='cartContainer'>
+        {state.map((item) => <div className='cartBox' key={item.id}>
+          <button onClick={()=>dispatch({type:"DeleteItem",payload:item.id})}>Delete</button>
+          <div><img src={item.image} height="60px" width="50px" alt={item.title} /></div>
+          <div>{(item.title).slice(0, 24)}</div>
+          <div>{item.price}</div>
+          <div>count</div>
+        </div>)
+        }
+      </div>
     </div>
   )
 }
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -6,9 +6,15 @@ export const Context = ({children}) => {
   
     const reducer=(state,action)=>{
         switch(action.type){
+            // add item
             case "AddToCart":
                 return ([...state,action.payload]);
-
+            // delete item
+            case "DeleteItem":
+              return (state.filter((item)=>item.id!==action.payload));
+            // reset cart
+            case "Reset" :
+              return state=[];
 
             default : return state;
         }
